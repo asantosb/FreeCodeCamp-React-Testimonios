5942d66d413a3eb532d6f946f3903084919a1545
sntsbrvl <alvarosantosb@gmail.com>
2022-03-23 12:18:19 +0100

    cambios

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -4,7 +4,7 @@ import '../css/Testimonio.css';
 function Testimonio(props) {
     return (
       <div className='contenedor-testimonio'>
-        <img className='imagen-testimonio' src={require(`../img/testimonio-${props.imagen}.png`)} alt='Foto' />
+        <img className='imagen-testimonio' src={require(`../img/testimonio-${props.imagen}.png`)} alt={`Foto de ${props.nombre}`} />
         <div className='contenedor-texto-testimonio'>
           <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
           <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
